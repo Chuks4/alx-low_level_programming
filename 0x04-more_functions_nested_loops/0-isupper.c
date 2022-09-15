ca5bf37db7c03b857c52f994ab91434669a83bfe
Chuks4 <chukwuemekaagha733@gmail.com>
2022-09-15 19:08:21 +0000

    isupper file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - a function that checks for uppercase character
+ * @c: input
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+char c;
+c = 'C';
+if (isupper(c))
+	putchar("return uppercase\n")
+else
+putchar("not uppercase")
+return(0)
+}
