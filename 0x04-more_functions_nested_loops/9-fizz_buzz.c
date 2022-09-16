2fd41ec9050339120bf8a287d3617c0d60036266
chuks4 <chukwuemekaagha733@gmail.com>
2022-09-16 22:33:38 +0000

    fizz buzz file

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0)
+		{
+			printf("fizz ");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (num % 3 == 0 && num % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%i", num);
+		}
+	}
+	putchar("\n")
+		return (0);
+}
