53b4652cc00a8cc353a8b9fb1bc6514e4105ea73
chuks4 <chukwuemekaagha733@gmail.com>
2022-09-19 21:59:19 +0000

    put file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints to the standard output
+ * @str: string to be printed to the standard output
+ * Return: Null
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
