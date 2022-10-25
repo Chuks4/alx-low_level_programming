73ec4236bb7532882daa778a58ec1472ddc980d3
chuks4 <chukwuemekaagha733@gmail.com>
2022-10-25 14:08:55 +0000

    Function that prints a listint_t linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,30 +1,90 @@
 #include "lists.h"
+#include <stdio.h>
+
+size_t looped_listint_len(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
 
 /**
- * reverse_listint - Reverses a listint_t list
- * @head: A pointer to the address of
- * the head of the list_t list.
+ * looped_listint_len - Counts the number of unique nodes
+ * in a looped listint_t linked list.
  *
- * Return: A pointer to the first node of the reversed list.
+ * @head: A pointer to the head of the listint_t to check.
+ *
+ * Return: If the list is not looped - 0.
+ * Otherwise - the number of unique nodes in the list.
  */
-listint_t *reverse_listint(listint_t **head)
+size_t looped_listint_len(const listint_t *head)
 {
-	listint_t *ahead, *behind;
+	const listint_t *slowP, *fastP;
+	size_t nodes = 1;
 
-	if (head == NULL || *head == NULL)
-		return (NULL);
+	if (head == NULL || head->next == NULL)
+		return (0);
 
-	behind = NULL;
+	slowP = head->next;
+	fastP = (head->next)->next;
 
-	while ((*head)->next != NULL)
+	while (fastP)
 	{
-		ahead = (*head)->next;
-		(*head)->next = behind;
-		behind = *head;
-		*head = ahead;
+		if (slowP == fastP)
+		{
+			slowP = head;
+			while (slowP != fastP)
+			{
+				nodes++;
+				slowP = slowP->next;
+				fastP = fastP->next;
+			}
+
+			slowP = slowP->next;
+			while (slowP != fastP)
+			{
+				nodes++;
+				slowP = slowP->next;
+			}
+
+			return (nodes);
+		}
+
+		slowP = slowP->next;
+		fastP = (fastP->next)->next;
 	}
 
-	(*head)->next = behind;
+	return (0);
+}
 
-	return (*head);
+
+/**
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nodes, index = 0;
+
+	nodes = looped_listint_len(head);
+
+	if (nodes == 0)
+	{
+		for (; head != NULL; nodes++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+	}
+
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	}
+
+	return (nodes);
 }
